f17774a7ae33209e2c8f2b95d9f028b0ef42b3ca
MaheshFSD <maheshfsdeveloper@gmail.com>
2021-09-19 22:16:34 +0530

    done filtering by date

--- src/Components/CarsList.jsx
+++ src/Components/CarsList.jsx
@@ -7,14 +7,12 @@ import EndOfData from "./EndOfData";
 const CarsList = () => {
     
   const [items, setItems] = useState([]);
-
   const [hasMore, sethasMore] = useState(true);
-
   const [page, setpage] = useState(1);
   const [date, setDate] = useState("");
   // const [filterOn,setFilterOn] = useState(false)
   // const [url,setUrl] = useState(false)
-  
+  console.log(items, " items values")
   useEffect(() => {
     const getCrashData = async () => {
       const res = await fetch(`https://data.cityofnewyork.us/resource/h9gi-nx95.json?$offset=0&$limit=10&$order=collision_id`);
@@ -26,7 +24,7 @@ const CarsList = () => {
     getCrashData();
 
   }, []);
-  console.log(date)
+  console.log(date," from top")
   const fetchCrashesAsScrollDown = async () => {
     const res = await fetch(`https://data.cityofnewyork.us/resource/h9gi-nx95.json?$offset=${page*10}&$limit=10&$order=collision_id`);
     const data = await res.json();
@@ -45,17 +43,21 @@ const CarsList = () => {
     setpage(page + 1);
     console.log('fetch data is getting called')
   };
-  const getDateFilteredData = async () => {
-    const res = await fetch(`https://data.cityofnewyork.us/resource/h9gi-nx95.json?$crash_date=${date}`);
+  const getDateFilteredData = async (day) => {
+    console.log(`https://data.cityofnewyork.us/resource/h9gi-nx95.json?crash_date=${day}`," from filter function ")
+    const res = await fetch(`https://data.cityofnewyork.us/resource/h9gi-nx95.json?crash_date=${day}`);
     const data = await res.json();
     setItems(data);
+    console.log(" from filter ",date)
     console.log(data , " from first call")
   };
   function handleDate(dat){
     console.log(" handle date :  ",dat)
+    console.log(typeof dat," type")
     let day=dat+"T00:00:00.000"
     setDate(day)
-    getDateFilteredData()
+    console.log(day , " from handle date after setting")
+    getDateFilteredData(day)
   }
     return (
       <div>
